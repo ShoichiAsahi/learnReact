03c01122ceaad06230759b66a0d3e800e9847902
roybatty <ignis19760616@gmail.com>
2020-04-27 16:19:14 +0900

    Final version

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({id, name, email}) => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>  {/*className come from tachyons*/}
+		<div className='tc bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5'>  {/*className come from tachyons*/}
 			<img alt='robots' src={`https://robohash.org/${id}?100*100`}/>
 			<div>
 				<h2>{name}</h2>
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import CardList from "./CardList"; 
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-import { robots } from "./robots";
-
-// we have to destructuring（分割構文）
-
 ReactDOM.render(
-  <CardList robots={ robots }/> 
+  <App />
   ,document.getElementById('root'));
 
-/*　robots のプロパティ（オブジェクトの　id, name, email）を受け取る */
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+  
